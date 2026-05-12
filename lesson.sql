d58fc2576ee5fabc499f6fae9380f38429ae05b2
MelissaMarques <melissa.marques1@nhs.net>
2026-05-12 11:13:36 +0000

    Refactor SQL queries in lesson.sql and add new exercises in CaseExercise.sql and CaseLesson-sql

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,20 @@
-/*
-Example lesson stub
-*/
-
 SELECT
-	*
+	ps.PatientId
+	,ps.AdmittedDate
+	,ps.DischargeDate
+	,ps.Hospital
+	,ps.Ward
+	,DATEDIFF(DAY, ps.AdmittedDate, ps.DischargeDate) + 1 AS LengthOfStay
+	,DATEADD (month, 3, ps.DischargeDate) AS AppointmentDate
+
 FROM
-	PatientStay ps ;
+	PatientStay ps
+
+WHERE
+ -- ps.Hospital IN ( 'Oxleas', 'PRUH' )
+ ps.AdmittedDate BETWEEN '2024-02-01' AND '2024-02-28'
+	AND ps.Ward LIKE 'D%'
+
+ORDER BY
+ ps.AdmittedDate DESC
+ , ps.PatientId DESC;
